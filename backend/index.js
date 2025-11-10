0b875fb09a4e5e33137e55409c89ffaf3a0a4502
ChaoBanhNguyen <102232031+Chaobanh27@users.noreply.github.com>
2025-11-11 02:52:08 +0700

    change file indexjs

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "../config/db.js";
+import router from "../routes/index.js";
 
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors({
-  origin: "*"
+  origin: "http://localhost:3000", // frontend local
+  credentials: true
 }));
-app.get('/', (req, res)=> res.send('Server is running'))
 
-app.use("/api",router)
+app.get("/", (req, res) => res.send("Server is running"));
+app.use("/api", router);
 
-const PORT = 8080 || process.env.PORT
-
-
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("connnect to DB")
-        console.log("Server is running "+PORT)
-    })
-})
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
