127834179ef02538b3e65852f0d4876455826d4e
ChaoBanhNguyen <102232031+Chaobanh27@users.noreply.github.com>
2025-11-11 02:39:28 +0700

    change cors

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-origin: ["http://localhost:3000"],
-    credentials : true
-}))
+  origin: "http://localhost:3000", // hoặc "*" để cho phép mọi origin
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.get('/', (req, res)=> res.send('Server is running'))
